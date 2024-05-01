b1d879391d783d908a462f25d499f2a4040743b1
Khairul Alam <khairulislam38@gmail.com>
2024-05-01 10:39:14 +0600

    first commit

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+// import Loader from "@/components/common/Loader";
+
+const Loading = () => {
+  return (
+    <div>
+      {/* <Loader className="h-[50vh] flex items-end justify-center" /> */}
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
