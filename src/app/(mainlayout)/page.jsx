5d10a74aa20e460c9e5e0eaa7bf0e15bb2b46f2a
Khairul Alam <khairulislam38@gmail.com>
2024-05-03 12:14:07 +0600

    update

--- src/app/(mainlayout)/page.jsx
+++ src/app/(mainlayout)/page.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import Navigation from "../../components/ui/Navigation"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>This is home page</h1>
-    </div>
+    <>
+      <div className="flex flex-col w-screen h-screen overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
+        <Navigation />
+        <div className="px-10 mt-6 flex justify-between">
+          <h1 className="text-2xl font-bold">Projects</h1>
+          <button
+            // onClick={controlModal}
+            className="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
+          >
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        {/* {content} */}
+      </div>
+      {/* <AddTeamModal
+        opened={opened}
+        controlModal={controlModal}
+        notify={notify}
+      /> */}
+      {/* <ToastContainer autoClose={3000} theme="colored" /> */}
+    </>
   );
 };
 
---
+++ src/components/ui/Navigation.jsx
@@ -0,0 +1,86 @@
+"use client"
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Navigation = () => {
+  const pathname = usePathname();
+  return (
+    <div className="flex items-center justify-between w-full h-16 px-10 bg-white bg-opacity-75">
+      <Link href="/">
+        {/* <img src={logoImage} className="h-10 w-10 cursor-pointer" alt="" /> */}
+        <svg width="32" height="32" viewBox="0 0 32 32">
+          <defs>
+            <linearGradient
+              x1="28.538%"
+              y1="20.229%"
+              x2="100%"
+              y2="108.156%"
+              id="logo-a"
+            >
+              <stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
+              <stop stopColor="#A5B4FC" offset="100%" />
+            </linearGradient>
+            <linearGradient
+              x1="88.638%"
+              y1="29.267%"
+              x2="22.42%"
+              y2="100%"
+              id="logo-b"
+            >
+              <stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
+              <stop stopColor="#38BDF8" offset="100%" />
+            </linearGradient>
+          </defs>
+          <rect fill="#6366F1" width="32" height="32" rx="16" />
+          <path
+            d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z"
+            fill="#4F46E5"
+          />
+          <path
+            d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z"
+            fill="url(#logo-a)"
+          />
+          <path
+            d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z"
+            fill="url(#logo-b)"
+          />
+        </svg>
+      </Link>
+
+      <div className="flex">
+        <Link href="/teams">
+          <p
+            className={`mx-2 text-sm font-semibold hover:text-indigo-700 ${
+              pathname === "/" ? "text-indigo-700" : "text-gray-600"
+            }`}
+          >
+            Home
+          </p>
+        </Link>
+        {/* <Link href="/projects">
+          <p
+            className={`mx-2 text-sm font-semibold hover:text-indigo-700 ${
+              pathname === "/projects" ? "text-indigo-700" : "text-gray-600"
+            }`}
+          >
+            Projects
+          </p>
+        </Link> */}
+      </div>
+      {/* {children} */}
+      {/* <button className="flex items-center justify-center w-8 h-8 ml-auto overflow-hidden rounded-full cursor-pointer">
+        <img
+          src="https://assets.codepen.io/5041378/internal/avatars/users/default.png?fit=crop&format=auto&height=512&version=1600304177&width=512"
+          alt="pathyhssdf asd "
+        />
+      </button> */}
+      <div className="flex items-center justify-center">
+        {/* <DropdownProfile user={user} handleLogout={handleLogout} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
